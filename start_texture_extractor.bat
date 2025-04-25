1061bc4221d3e346f9aceeda20f5b54cba0f33aa
BTSpaniel <btspaniel@gmail.com>
2025-04-25 12:49:54 -0400

    Add Python version checker and installer script. Update start_texture_extractor.bat to use Python 3.13.3

--- start_texture_extractor.bat
+++ start_texture_extractor.bat
@@ -1,3 +1,6 @@
 @echo off
-cd desktop
-python main.py
+:: Change to the directory where this batch file is located
+cd /d "%~dp0"
+
+:: Check Python installation, install/update if needed, and run the application
+call check_and_install_python.bat
